72cc07e9ba815a620558095188626b6ec01ffd41
masp005 <spelsberg.marcel@gmail.com>
2023-04-29 03:27:47 +0200

    web app working

--- index.js
+++ index.js
@@ -42,8 +42,45 @@ const app = expr();
 
 app.set("view engine", "ejs");;
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.render("dash", { spells });
+});
+app.post("/", expr.json(), (req, res) => {
+  let { method, spell, response, data } = req.body;
+  console.log({ method, spell, response, data });
+  if (typeof spell != "undefined") spell = parseInt(spell.trim());
+  if (typeof response != "undefined") response = parseInt(response.trim());
+  if (!method) return res.sendStatus(400);
+  const s = spells[spell];
+  switch (method) {
+    case "create":
+      if (typeof spell == "undefined" || typeof response == "undefined") return res.sendStatus(400);
+      spells.push(new Spell({
+        trigger: spell,
+        responses: [response]
+      }));
+      break;
+    case "weight":
+      if (typeof spell == "undefined" || typeof response == "undefined" || typeof data == "undefined") return res.sendStatus(400);
+      if (response >= s.responses.length) s.responses[response] = {};
+      if (!data) s.responses.splice(response, 1);
+      else s.responses[response].weight = parseFloat(data);
+      break;
+    case "spell":
+      if (typeof spell == "undefined" || typeof data == "undefined") return res.sendStatus(400);
+      s.trigger = new RegExp(data, "i");
+      break;
+    case "response":
+      if (typeof spell == "undefined" || typeof response == "undefined" || typeof data == "undefined") return res.sendStatus(400);
+      if (typeof s.responses[response] == "undefined") s.responses[response] = {};
+      s.responses[response].text = data.trim();
+      break;
+    case "delete":
+      if (typeof spell == "undefined") return res.sendStatus(400);
+      spells.splice(spell, 1);
+      break;
+  }
+  res.sendStatus(204);
 });
 
 app.listen(PORT);
